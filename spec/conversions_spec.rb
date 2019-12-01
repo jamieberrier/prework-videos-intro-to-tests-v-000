3d6562ec60cd70bb35bfdf77c9311b5c79e27389
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 17:31:38 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -6,6 +6,7 @@ describe "conversions" do
       grams = ounces_to_grams(0)
       expect(grams).to eq(0.0)
     end
+    
     it 'given 1, returns 28.3495' do
       grams = ounces_to_grams(1)
       expect(grams).to eq(28.3495)
